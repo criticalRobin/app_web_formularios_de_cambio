9b9f4a3d79167ed2babc4ac4f0a11eda0767946e
Kenneth <kparedes9203@uta.edu.ec>
2023-11-30 01:06:38 -0500

    Modificacion de rutas correctas a las pagians web y cambio de estilos en los ingresos de los social accounts

--- change_forms_web_app/urls.py
+++ change_forms_web_app/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path, include
 
-def home(request):
+def login(request):
     return render(request,'login.html')
-
+def profile(request):
+    return render(request,'home.html')
 urlpatterns = [
-    path('',home),
+    path('',login),
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
+    path('accounts/profile/',profile)
 ]
